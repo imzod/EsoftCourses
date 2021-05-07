fe7723c7f5364e0465b009432ac5ba64c3228d6b
imzod <imzod@yandex.ru>
2021-05-07 17:35:24 +0500

    2

---
+++ udemy/20/app.js
@@ -0,0 +1,56 @@
+/*function singSong() {
+    console.log("DO")
+    console.log("RE")
+    console.log("MI")
+}
+
+function greet(firstName) {
+    console.log(`firstname is: ${firstName}`)
+    console.log('HI!')
+}
+
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}*/
+
+/*
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+*/
+/*function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}*/
+
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
